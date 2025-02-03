eb184af6322e3081f5b14f6622870170d65a8dee
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-02-03 21:53:00 +0530

    Delete & get requests for Events

--- CG_backend/Routes/EventRoute.js
+++ CG_backend/Routes/EventRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getEventList,postEvent } from '../Controller/Events.Controller/Events.Controller.js'
+import { getEventList,postEvent,deleteEvent} from '../Controller/Events.Controller/Events.Controller.js'
 
 
 const EventRouter = express.Router()
 
 EventRouter.get('/', getEventList)
 EventRouter.post('/', postEvent)
+EventRouter.delete('/', deleteEvent)
 
 
 
