1fdc8b18e2d968f6aa7b728f183a2bf14c8a7580
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-02-18 20:15:12 +0530

    Fixed Routes in App.jsx

--- CG_frontend/src/App.jsx
+++ CG_frontend/src/App.jsx
@@ -10,6 +10,7 @@ import LandingPage from './Modules/LandingPage/LandingPage';
 import Newsletter from './Modules/NewsletterPage/Newsletter';
 import Revamp from './Modules/Revamp';
 import Portfolio from './Modules/Portfolio';
+import ProtectedRoute from './Modules/ProtectedRoute';
 
 
 function App() {
@@ -17,25 +18,29 @@ function App() {
   return (
     <>
       <Router>
+        <Navbar /> {/* Navbar should be placed outside Routes */}
         <Routes>
-          
           <Route path="/cg" element={<Revamp />} />
-          <Route path="/cg/port" element={<Portfolio />} />
 
-          <Route element={<Navbar />}>
+          {/* Protected Route Wrapping Portfolio */}
+          <Route
+            path="/cg/port"
+            element={
+              <ProtectedRoute>
+                <Portfolio />
+              </ProtectedRoute>
+            }
+          />
 
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/event" element={<Events />} />
-            <Route path="/activity" element={<ActivityPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/newsletter" element={<Newsletter />} />
-
-          </Route>
-
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          
+          <Route path="/event" element={<Events />} />
+          <Route path="/activity" element={<ActivityPage />} />
+          <Route path="/newsletter" element={<Newsletter />} />
         </Routes>
       </Router>
-
     </>
   )
 }
